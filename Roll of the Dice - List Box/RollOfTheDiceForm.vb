a2bb67bccabcb5c7bed2b796d41db5add6f34058
Zac <zac.minidude140@Gmail.com>
2023-10-29 11:50:51 -0600

    Converted methods with console commands **UNTESED**

--- Roll of the Dice - List Box/RollOfTheDiceForm.vb
+++ Roll of the Dice - List Box/RollOfTheDiceForm.vb
@@ -7,7 +7,7 @@
 'TODO:
 '[~]Import Roll of the dice methods (small enough will copy - paste)
 '[~]Convert Methods without console commands
-'[]Convert Methods with console commands to update string (create display string)
+'[~]Convert Methods with console commands to update string (create display string) Next time use accumulate message function
 '[]Convert Methods into the event handlers from console statement tree (ROll actions, clear actions, update display)
 
 Public Class RollOfTheDiceForm
@@ -16,6 +16,8 @@ Public Class RollOfTheDiceForm
     Dim diceTotal As Integer
     Dim diceRollTally() As Integer = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
     Dim numberOfRolls As Integer = 0
+    Dim display As String = ""
+    Dim newDisplay As String = ""
 
     'Custom Methods
     ''' <summary>
@@ -40,6 +42,72 @@ Public Class RollOfTheDiceForm
         summation = numberOne + numberTwo
         Return summation
     End Function
+
+    ''' <summary>
+    '''Evaluates diceTotal and increments corresponding array element
+    ''' </summary>
+    ''' <param name="diceTotal"></param>
+    Sub EvaluateTotal(diceTotal As Integer)
+        Select Case diceTotal
+            Case = 2
+                diceRollTally(0) += 1
+            Case = 3
+                diceRollTally(1) += 1
+            Case = 4
+                diceRollTally(2) += 1
+            Case = 5
+                diceRollTally(3) += 1
+            Case = 6
+                diceRollTally(4) += 1
+            Case = 7
+                diceRollTally(5) += 1
+            Case = 8
+                diceRollTally(6) += 1
+            Case = 9
+                diceRollTally(7) += 1
+            Case = 10
+                diceRollTally(8) += 1
+            Case = 11
+                diceRollTally(9) += 1
+            Case = 12
+                diceRollTally(10) += 1
+        End Select
+    End Sub
+
+    ''' <summary>
+    ''' Updates display string with given array data
+    ''' </summary>
+    ''' <param name="diceRollTally"></param>
+    Sub DrawDiceRolls(diceRollTally() As Integer)
+        'create header with possible rolls
+        Dim header = New String() {"2  |", "3  |", "4  |", "5  |", "6  |", "7  |", "8  |", "9  |", "10  |", "11  |", "12  |"}
+        'Draw out top border and header
+        newDisplay = StrDup(78, "-") & "|"
+        display = display & newDisplay
+        For i = LBound(header) To UBound(header)
+            newDisplay = header(i).PadLeft(7)
+            display = display & newDisplay
+        Next
+        'Draw out diceRollTally() with borders
+        newDisplay = vbCrLf & vbCrLf & StrDup(78, "-") & "|"
+        display = display & newDisplay
+        For i = LBound(diceRollTally) To UBound(diceRollTally)
+            Dim currentTotal As String
+            currentTotal = CStr(diceRollTally(i) & "  |")
+            newDisplay = currentTotal.PadLeft(7)
+            display = display & newDisplay
+        Next
+        newDisplay = vbCrLf & vbCrLf & StrDup(78, "-") & vbCrLf
+        display = display & newDisplay
+        'Reports the total number of rolls after array is drawn
+        If numberOfRolls = 0 Then
+            newDisplay = vbCrLf & "Total Rolls: 0" & vbCrLf
+            display = display & newDisplay
+        Else
+            newDisplay = vbCrLf & $"Total Rolls: {numberOfRolls},000" & vbCrLf
+            display = display & newDisplay
+        End If
+    End Sub
 
     'Event Handlers
     Private Sub ExitButton_Click(sender As Object, e As EventArgs) Handles ExitButton.Click
@@ -76,63 +144,6 @@ End Class
 '                    DrawDiceRolls(diceRollTally)
 '            End Select
 '        Loop
-'    End Sub
-
-
-
-'    Sub DrawDiceRolls(diceRollTally() As Integer)
-'        'create header with possible rolls
-'        Dim header = New String() {"2  |", "3  |", "4  |", "5  |", "6  |", "7  |", "8  |", "9  |", "10  |", "11  |", "12  |"}
-'        'Draw out top border and header
-'        Console.WriteLine(StrDup(78, "-"))
-'        Console.Write("|")
-'        For i = LBound(header) To UBound(header)
-'            Console.Write(header(i).PadLeft(7))
-'        Next
-'        'Draw out diceRollTally() with borders
-'        Console.WriteLine(vbLf & StrDup(78, "-"))
-'        Console.Write("|")
-'        For i = LBound(diceRollTally) To UBound(diceRollTally)
-'            Dim currentTotal As String
-'            currentTotal = CStr(diceRollTally(i) & "  |")
-'            Console.Write(currentTotal.PadLeft(7))
-'        Next
-'        Console.WriteLine(vbLf & StrDup(78, "-") & vbLf)
-'        'Reports the total number of rolls after array is drawn
-'        If numberOfRolls = 0 Then
-'            Console.WriteLine("Total Rolls: 0" & vbLf)
-'        Else
-'            Console.WriteLine($"Total Rolls: {numberOfRolls},000" & vbLf)
-'        End If
-
-'    End Sub
-
-'    Sub EvaluateTotal(diceTotal As Integer)
-'        'evaluates diceTotal and increments corresponding array element
-'        Select Case diceTotal
-'            Case = 2
-'                diceRollTally(0) += 1
-'            Case = 3
-'                diceRollTally(1) += 1
-'            Case = 4
-'                diceRollTally(2) += 1
-'            Case = 5
-'                diceRollTally(3) += 1
-'            Case = 6
-'                diceRollTally(4) += 1
-'            Case = 7
-'                diceRollTally(5) += 1
-'            Case = 8
-'                diceRollTally(6) += 1
-'            Case = 9
-'                diceRollTally(7) += 1
-'            Case = 10
-'                diceRollTally(8) += 1
-'            Case = 11
-'                diceRollTally(9) += 1
-'            Case = 12
-'                diceRollTally(10) += 1
-'        End Select
 '    End Sub
 
 'End Module
