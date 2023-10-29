3cc458a7efc4de0770093fc45d0349ce0efcd918
Zac <zac.minidude140@Gmail.com>
2023-10-29 10:51:40 -0600

    Added user GUI and Exit functionality

--- Roll of the Dice - List Box/RollOfTheDiceForm.Designer.vb
+++ Roll of the Dice - List Box/RollOfTheDiceForm.Designer.vb
@@ -22,10 +22,66 @@ Partial Class RollOfTheDiceForm
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.RollButton = New System.Windows.Forms.Button()
+        Me.ClearButton = New System.Windows.Forms.Button()
+        Me.ExitButton = New System.Windows.Forms.Button()
+        Me.DisplayRollsListBox = New System.Windows.Forms.ListBox()
+        Me.SuspendLayout()
+        '
+        'RollButton
+        '
+        Me.RollButton.Location = New System.Drawing.Point(130, 344)
+        Me.RollButton.Name = "RollButton"
+        Me.RollButton.Size = New System.Drawing.Size(165, 83)
+        Me.RollButton.TabIndex = 0
+        Me.RollButton.Text = "&Roll"
+        Me.RollButton.UseVisualStyleBackColor = True
+        '
+        'ClearButton
+        '
+        Me.ClearButton.Location = New System.Drawing.Point(301, 344)
+        Me.ClearButton.Name = "ClearButton"
+        Me.ClearButton.Size = New System.Drawing.Size(165, 83)
+        Me.ClearButton.TabIndex = 1
+        Me.ClearButton.Text = "&Clear"
+        Me.ClearButton.UseVisualStyleBackColor = True
+        '
+        'ExitButton
+        '
+        Me.ExitButton.Location = New System.Drawing.Point(472, 344)
+        Me.ExitButton.Name = "ExitButton"
+        Me.ExitButton.Size = New System.Drawing.Size(165, 83)
+        Me.ExitButton.TabIndex = 2
+        Me.ExitButton.Text = "E&xit"
+        Me.ExitButton.UseVisualStyleBackColor = True
+        '
+        'DisplayRollsListBox
+        '
+        Me.DisplayRollsListBox.FormattingEnabled = True
+        Me.DisplayRollsListBox.ItemHeight = 16
+        Me.DisplayRollsListBox.Location = New System.Drawing.Point(45, 35)
+        Me.DisplayRollsListBox.Name = "DisplayRollsListBox"
+        Me.DisplayRollsListBox.Size = New System.Drawing.Size(722, 260)
+        Me.DisplayRollsListBox.TabIndex = 3
+        Me.DisplayRollsListBox.TabStop = False
+        '
+        'RollOfTheDiceForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "Form1"
+        Me.Controls.Add(Me.DisplayRollsListBox)
+        Me.Controls.Add(Me.ExitButton)
+        Me.Controls.Add(Me.ClearButton)
+        Me.Controls.Add(Me.RollButton)
+        Me.Name = "RollOfTheDiceForm"
+        Me.Text = "Roll of the Dice"
+        Me.ResumeLayout(False)
+
     End Sub
 
+    Friend WithEvents RollButton As Button
+    Friend WithEvents ClearButton As Button
+    Friend WithEvents ExitButton As Button
+    Friend WithEvents DisplayRollsListBox As ListBox
 End Class
--- Roll of the Dice - List Box/RollOfTheDiceForm.vb
+++ Roll of the Dice - List Box/RollOfTheDiceForm.vb
@@ -5,5 +5,10 @@
 'https://github.com/Minidude140
 
 Public Class RollOfTheDiceForm
+    'Custom Methods
 
+    'Event Handlers
+    Private Sub ExitButton_Click(sender As Object, e As EventArgs) Handles ExitButton.Click
+        Me.Close()
+    End Sub
 End Class
